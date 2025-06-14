f75cc5a5e1eccb8ff6539451a5d71c0532a726fc
Ayush Kumar <kumarayushkumar@protonmail.com>
2025-06-14 20:39:39 +0530

    chore: replace Aber Mono fonts with Source Code Pro; add Books and Screens component; implement contact data; create project data; update layout and index page

---
+++ src/data/book-and-screen.ts
@@ -0,0 +1,29 @@
+export const books = [
+  'Before you startup',
+  'How to win friends and influence people',
+  'The Design of everyday things',
+  'Building a story brand',
+  'Exactly what to say',
+  'Never split the difference',
+  'Designing for Emotion',
+  'Microinteractions',
+  "Don't make me think",
+  'Laws of UX',
+  'Refactoring UI',
+  'Thinking with Type',
+  'User friendly',
+  'Universal Principles of Design',
+]
+
+export const screens = [
+  'The Social Network',
+  'The Imitation Game',
+  'The Founder',
+  'Moneyball',
+  'Steve Jobs',
+  'The Big Short',
+  'Pirates of Silicon Valley',
+  'How to become a tyrant',
+  'Nike',
+  'The Billion Dollar Code'
+]
---
+++ src/data/project.ts
@@ -0,0 +1,19 @@
+import test from '../assets/images/test.png'
+
+export const project = [
+  {
+    name: 'Razemble',
+    description: 'LinkedIn for projects',
+    image: test
+  },
+  {
+    name: 'Travkaro',
+    description: 'Instagram for travel',
+    image: test
+  },
+  {
+    name: 'Emersym',
+    description: "Woman's safety using Deep learning, Won 3 hackathons.",
+    image: test
+  }
+]
