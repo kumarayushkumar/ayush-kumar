810456d798b9e62a16eb231c8acff9767b1931ab
Ayush Kumar <kumarayushkumar@protonmail.com>
2025-06-18 12:56:46 +0530

    feat: add new fonts and images; update global styles and component typography

--- src/data/product.ts
+++ src/data/product.ts
@@ -3,16 +3,12 @@ import test from '../assets/images/test.png'
 export const product = [
   {
     name: 'Comment AI',
-    description:
-      'Comments on 20+ in 5 minutes; LinkedIn algorithm loves people who engage with others. So comment.',
     liveLink: '',
     image: test,
     downloads: 0
   },
   {
     name: 'Hire Ai',
-    description:
-      'Analyse and filter resumes with your custom criteria within 2 minutes.',
     liveLink: '',
     image: test,
     downloads: 0
--- src/data/project.ts
+++ src/data/project.ts
@@ -1,19 +1,21 @@
+import razemble from '../assets/images/razemble.png'
 import test from '../assets/images/test.png'
+import travkaro from '../assets/images/travkaro.png'
 
 export const project = [
   {
-    name: 'Razemble',
-    description: 'LinkedIn for projects',
-    image: test
-  },
-  {
     name: 'Travkaro',
     description: 'Instagram for travel',
-    image: test
+    image: travkaro
   },
   {
     name: 'Emersym',
     description: "Woman's safety using Deep learning, Won 3 hackathons.",
     image: test
+  },
+  {
+    name: 'Razemble',
+    description: 'LinkedIn for projects',
+    image: razemble
   }
 ]
