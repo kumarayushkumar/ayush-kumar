b991c6f3ece10ee0b689d708541e2d76ad47ac0c
Ayush Kumar <kumarayushkumar@protonmail.com>
2025-07-22 12:26:00 +0530

    fix: remove phone number from contact information and clean up social links

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,10 +1,8 @@
 export const contact = {
   email: 'kumarayushkumar@protonmail.com',
-  phone: '7440532769',
   social: {
     twitter: 'https://x.com/ayushkumarkeirn',
     linkedin: 'https://www.linkedin.com/in/ayushkumarkumar/',
     github: 'https://github.com/kumarayushkumar',
-    instagram: 'https://www.instagram.com/ayushkumarkeirn'
   }
 }
