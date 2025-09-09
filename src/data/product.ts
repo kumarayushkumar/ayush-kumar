a010fe9179533973e46f93e3a49925336b01a444
Ayush Kumar <kumarayushkumar@protonmail.com>
2025-09-09 14:09:21 +0530

    feat: added blog functionality, wrote new blog and code refactor

--- src/data/product.ts
+++ src/data/product.ts
@@ -13,25 +13,28 @@ type Product = {
 export const product: Product[] = [
   {
     name: 'Travkaro',
-    description: "Instagram for travel, dropped it in between. It impossible to bring people on a new social media without a lot of money",
+    description:
+      'Instagram for travel, dropped it in between. It impossible to bring people on a new social media without a lot of money',
     image: travkaro
   },
   {
     name: 'Post AI',
     liveLink: 'https://postai.space',
     description: 'AI powered social media post generator',
-    image: postai,
+    image: postai
   },
   {
     name: 'UI Analyzer',
     liveLink: 'https://uianalyzer.in',
-    description: "Analyse your website's UI and get suggestions to improve it. It's Open Source project",
-    image: uianalyzer,
+    description:
+      "Analyse your website's UI and get suggestions to improve it. It's Open Source project",
+    image: uianalyzer
   },
-   {
+  {
     name: 'Curlme',
     liveLink: 'https://curlme.dev',
-    description: "Place to vibe with other coder only from terminal twitter from terminal but only for developers.",
-    image: curlme,
+    description:
+      'Place to vibe with other coder only from terminal twitter from terminal but only for developers.',
+    image: curlme
   }
 ]
